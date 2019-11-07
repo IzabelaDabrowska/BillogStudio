99c7f340b6d0a4ef05f45a16533c64eef9f704ab
Izabela Dabrowska <dabrowska.izabelaa@wp.pl>
2019-11-07 10:46:39 +0100

    changed controller and icon of cup, added closing hamburgerafter click

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,19 @@
 window.onload = function () {
     const hamburger = document.getElementById('hamburger');
+    const navigation = document.getElementById('navigation');
+    const navBtn = document.querySelectorAll('.nav-button');
+
     hamburger.addEventListener('click', function () {
         hamburger.classList.toggle('is-active');
+        navigation.classList.toggle('nav-active');
+    });
+    console.log(navBtn);
+
+    navBtn.forEach(item => {
+        item.addEventListener('click', event => {
+          hamburger.classList.remove('is-active');
+          navigation.classList.remove('nav-active');
+        });
     });
 
     function scroll(e) {
